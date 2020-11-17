3660fa2beb4fb32dca1a25ad102643fb949d79fd
Cesar <ca6fqb@virginia.edu>
2020-11-17 17:29:45 -0500

    final version

---
+++ assets/php/dbh.php
@@ -0,0 +1,9 @@
+<?php
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassward = "";
+$dbName = "login_test";
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassward,$dbName);
+
+?>
